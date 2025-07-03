16bd08cda9abb61ddc9a0198b757035791d54537
SM <119601732+Alexbots1@users.noreply.github.com>
2025-07-03 15:50:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,7 +112,7 @@ LANGUAGES = ["malayalam", "", "tamil", "", "english", "", "hindi", "", "telugu",
 QUALITIES = ["360P", "", "480P", "", "720P", "", "1080P", "", "1440P", "", "2160P", ""]
 SEASONS = ["s01" , "s02" , "s03" , "s04", "s05" , "s06" , "s07" , "s08" , "s09" , "s10"]
 
-STREAM_MODE = bool(environ.get('STREAM_MODE', False))
+STREAM_MODE = bool(environ.get('STREAM_MODE', True))
 
 NO_PORT = bool(environ.get('NO_PORT', False))
 APP_NAME = None
